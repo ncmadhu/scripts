19ea726bbbcd022aea335fd9ea7de2f07f376922
Madhu Chakravarthy <ncmadhu@users.noreply.github.com>
2022-04-12 10:05:00 -0700

    Update mac_connect_wifi.sh
    Parametrized the script

--- mac_connect_wifi.sh
+++ mac_connect_wifi.sh
@@ -1,1 +1,13 @@
-while true ; networksetup -setairportnetwork en1 "PSK750" "PSKAP750" ; echo "###############################" ; networksetup -getairportnetwork en1 ; echo "###############################" ; do ping -c1 8.8.8.8 ; sleep 60 ; done
+#while true ; networksetup -setairportnetwork en1 "PSK750" "PSKAP750" ; echo "###############################" ; networksetup -getairportnetwork en1 ; echo "###############################" ; do ping -c1 8.8.8.8 ; sleep 60 ; done
+IFACE = "en1"
+SSID = "PSK750"
+PASSWD = "PSKAP750"
+while true
+do
+   networksetup -setairportnetwork $IFACE $SSID $PASSWD
+   echo "###############################"
+   networksetup -getairportnetwork $IFACE
+   echo "###############################"
+   ping -c1 8.8.8.8
+   sleep 60
+done
