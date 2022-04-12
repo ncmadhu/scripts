2170dfb765ebeafddacc7cac19d8ebcc77b61efe
Madhu Chakravarthy <ncmadhu@users.noreply.github.com>
2022-04-12 10:11:46 -0700

    Update mac_connect_wifi.sh
    Removed script errors

--- mac_connect_wifi.sh
+++ mac_connect_wifi.sh
@@ -1,7 +1,6 @@
-#while true ; networksetup -setairportnetwork en1 "PSK750" "PSKAP750" ; echo "###############################" ; networksetup -getairportnetwork en1 ; echo "###############################" ; do ping -c1 8.8.8.8 ; sleep 60 ; done
-IFACE = "en1"
-SSID = "PSK750"
-PASSWD = "PSKAP750"
+IFACE="en1"
+SSID="PSK750"
+PASSWD="PSKAP750"
 while true
 do
    networksetup -setairportnetwork $IFACE $SSID $PASSWD
